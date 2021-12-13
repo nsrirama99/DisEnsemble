81b146207e61b6ec52f12a266a930811a1b75412
williamchan8 <chanw@smu.edu>
2021-12-12 23:43:46 -0600

    Trained classifiers

--- turi-create models/classifier_generator.py
+++ turi-create models/classifier_generator.py
@@ -19,24 +19,30 @@ def check_for_label(audio_path, label):
   with open(labels_path) as f:
     return label in f.read()
 
-for name, label in instruments:
-  # Load and label the training data
-  train = tc.load_audio("IRMAS-TrainingData", with_path=True, recursive=True)
-  train["path"] = ['[' + label + ']' in path for path in train["path"]]
+
+# Load training data
+train = tc.load_audio("IRMAS-TrainingData", with_path=True, recursive=True)
+
+# Calculate deep features
+train["deep_features"] = tc.sound_classifier.get_deep_features(train["audio"])
+
+for name, label in instruments.items():
+  # Label the training data for target instrument
+  train["target"] = ['[' + label + ']' in path for path in train["path"]]
   print(train)
 
   # Load and label the testing data
-  test = tc.load_audio("IRMAS-TestingData", with_path=True)
-  test["path"] = [check_for_label(path, label) for path in test["path"]]
-  print(test)
+  # test = tc.load_audio("IRMAS-TestingData", with_path=True)
+  # test["path"] = [check_for_label(path, label) for path in test["path"]]
+  # print(test)
 
   # Create and train the model
-  model = tc.sound_classifier.create(train, target="path", feature="audio", batch_size=1024)
+  model = tc.sound_classifier.create(train, target="target", feature="deep_features", batch_size=1024, validation_set=None)
   print(model)
 
   # Evaluate model
-  metrics = model.evaluate(test)
-  print(metrics)
+  # metrics = model.evaluate(test)
+  # print(metrics)
 
   # Export to CoreML
   model.export_coreml(name + "_classifier.mlmodel")
