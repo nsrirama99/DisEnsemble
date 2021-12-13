f00639c4043edcde7706ec342441be53c246251f
Djaenk <wanganthony.wang@gmail.com>
2021-12-13 09:28:39 -0600

    Modified feature extraction order

--- turi-create models/classifier_generator.py
+++ turi-create models/classifier_generator.py
@@ -19,20 +19,21 @@ def check_for_label(audio_path, label):
   with open(labels_path) as f:
     return label in f.read()
 
-
-# Load training data
-train = tc.load_audio("IRMAS-TrainingData", with_path=True, recursive=True)
+# Calculate deep features in testing data
 test = tc.load_audio("IRMAS-TestingData", with_path=True, recursive=True)
-
-# Calculate deep features
-train["deep_features"] = tc.sound_classifier.get_deep_features(train["audio"])
 test["deep_features"] = tc.sound_classifier.get_deep_features(test["audio"])
+
+# Calculate deep features in training data
+train = tc.load_audio("IRMAS-TrainingData", with_path=True, recursive=True)
+train["deep_features"] = tc.sound_classifier.get_deep_features(train["audio"])
 
 for name, label in instruments.items():
   # Label the training data for target instrument
   train["target"] = ['[' + label + ']' in path for path in train["path"]]
   test["target"] = [check_for_label(path, label) for path in test["path"]]
   data = train.append(test)
+  if name == "drums":
+    data = train
   print(data)
 
   # Create and train the model
